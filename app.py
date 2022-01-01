6999fd5e585df71f5948c72b1cab6815df59ddde
atharvajainak <61925629+atharvajainak@users.noreply.github.com>
2022-01-01 12:10:25 +0530

    ci commit

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ sw = st.slider('Sepal Width', 0.5, 4.4, 2.8)
 pl = st.slider('Petal Length', 0.5, 6.9, 5.6)
 pw = st.slider('Petal Width', 0.1,2.5, 2.1)
 
+
 op = model.predict([[sl,sw,pl,pw]])
 st.title(op[0])
